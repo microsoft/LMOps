1e3bf11d92296e309049742ca1267eb2de9a71df
B. Dab <42484395+QuickSolverDab@users.noreply.github.com>
2024-04-16 08:08:17 +0900

    Update lm_datasets.py
    extend for the models that use uint32 bin-file

--- minillm/data_utils/lm_datasets.py
+++ minillm/data_utils/lm_datasets.py
@@ -57,6 +57,10 @@ class LMTrainDataset(Dataset):
         prompt = None
         if 65535 in input_ids:
             source_len = np.where(input_ids==65535)[0][0]
+            prompt = input_ids[:source_len] #for uint16 (others)
+            input_ids = np.concatenate([input_ids[:source_len], input_ids[source_len+1:]], axis=0)
+        elif 4294967295 in input_ids: #for uint32 (qwen, gemma, and etc)
+            source_len = np.where(input_ids==4294967295)[0][0]
             prompt = input_ids[:source_len]
             input_ids = np.concatenate([input_ids[:source_len], input_ids[source_len+1:]], axis=0)
         input_ids = input_ids[:self.max_length]
