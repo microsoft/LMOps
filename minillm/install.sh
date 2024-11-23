3fddecde3a3be8f4d5c145e77f9adc0735377034
Yuxian Gu <guyx21@mails.tsinghua.edu.cn>
2024-11-23 01:37:02 -0500

    update env

--- minillm/install.sh
+++ minillm/install.sh
@@ -1,15 +1,11 @@
 export NCCL_DEBUG=""
-pip3 install -e transformers/
-pip3 install torch==2.0.1
-pip3 install deepspeed==0.10.0
-pip3 install torchvision==0.15.2
-pip3 install nltk
+pip3 install https://github.com/t1101675/transformers.git@minillm
+pip3 install torch
+pip3 install deepspeed
 pip3 install numerize
 pip3 install rouge-score
 pip3 install torchtyping
 pip3 install rich
 pip3 install accelerate
 pip3 install datasets
-pip3 install sentencepiece
-pip3 install protobuf==3.20.3
 pip3 install peft
