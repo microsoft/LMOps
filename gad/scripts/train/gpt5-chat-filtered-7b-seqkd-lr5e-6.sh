7ad03c176dce8351b3508e1e3906a8b348bd6816
LeoYe <yetianzhu2022@gmail.com>
2025-12-02 01:39:59 -0800

    GAD add data preparation script

--- gad/scripts/train/gpt5-chat-filtered-7b-seqkd-lr5e-6.sh
+++ gad/scripts/train/gpt5-chat-filtered-7b-seqkd-lr5e-6.sh
@@ -33,8 +33,8 @@ export HYDRA_FULL_ERROR=1
 python3 -m verl.trainer.main_ppo \
     algorithm.adv_estimator=grpo \
     data.prompt_key=content \
-    data.train_files=/tmp/lmsys_gpt5_chat_4k_filtered_train.parquet \
-    data.val_files=/tmp/lmsys_gpt5_chat_4k_filtered_test.parquet \
+    data.train_files=/tmp/lmsys_gpt5_chat_filtered_train.parquet \
+    data.val_files=/tmp/lmsys_gpt5_chat_filtered_test.parquet \
     data.train_batch_size=256 \
     data.val_batch_size=600 \
     data.max_prompt_length=2048 \
